daa39493d9da7edfeef004e12ba4f5e68fc26fcf
Michael Boren <mboren@cooksys.com>
2016-07-25 06:15:30 -0400

    Location sql added to projecct

---
+++ src/main/resources/location.sql
@@ -0,0 +1,4 @@
+insert into ftd_flight.location values (1, 28.5383, -81.3792, 'Orlando');
+insert into ftd_flight.location values (2, 30.3322, -81.6557, 'Jacksonville');
+insert into ftd_flight.location values (3, 25.7617, -80.1918, 'Miami');
+insert into ftd_flight.location values (4, 30.4383, -84.2807, 'Tallahassee');

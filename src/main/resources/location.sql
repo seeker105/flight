14d5a28204c3d30b9773755b163b5827e8ceded2
Michael Boren <mboren@cooksys.com>
2016-07-25 08:37:42 -0400

    fix'd the sql statement

--- src/main/resources/location.sql
+++ src/main/resources/location.sql
@@ -1,4 +1,4 @@
-insert into ftd_flight.location values (1, 28.5383, -81.3792, 'Orlando');
-insert into ftd_flight.location values (2, 30.3322, -81.6557, 'Jacksonville');
-insert into ftd_flight.location values (3, 25.7617, -80.1918, 'Miami');
-insert into ftd_flight.location values (4, 30.4383, -84.2807, 'Tallahassee');
+insert into ftd_flight.location(id, latitude, longitude, city) values (1, 28.5383, -81.3792, 'Orlando');
+insert into ftd_flight.location(id, latitude, longitude, city) values (2, 30.3322, -81.6557, 'Jacksonville');
+insert into ftd_flight.location(id, latitude, longitude, city) values (3, 25.7617, -80.1918, 'Miami');
+insert into ftd_flight.location(id, latitude, longitude, city) values (4, 30.4383, -84.2807, 'Tallahassee');
